5e2e3d9ec70d93f7118d2c1b75d0c6d537def02e
Gaurav Rai <gauravrai1912@gmail.com>
2021-01-13 15:06:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import ShippingForm from './views/ShippingForm';
+import ListShippings from './views/ListShippings' ;
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <NavBar navLink={{ path: "/orders", Lable: "View Orders" }} />
+            <ShippingForm />
+          </Route>
+          <Route path="/orders">
+            <NavBar navLink={{ path: "/", Lable: "Home" }} />
+            <ListShippings />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
